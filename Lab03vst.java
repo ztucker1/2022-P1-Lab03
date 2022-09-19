3061abc68a9fc32e844cf8e99b81fc25e3550703
13433 <natalie.tucker@vtsd.com>
2022-09-19 09:29:28 -0400

    done?

--- Lab03vst.java
+++ Lab03vst.java
@@ -9,7 +9,17 @@ public class Lab03vst
 	public static void main(String[] args)
 	{
       System.out.println("Lab03, 80 Point Version\n");
-      
+	  int milliseconds = 10000123;
+		int hours = milliseconds/3600000;
+		int min = 2800/60;
+		int second = milliseconds/60/60/60-6;
+		int msec = milliseconds-10000000;
+		System.out.println("Starting milli-seconds: " + milliseconds);
+		System.out.println("Hours:                  " + hours);
+		System.out.println("Minutes:                " + min);
+		System.out.println("Seconds:                " +second);
+		System.out.println("Milli Seconds:          " + msec);
+
 	}
 }
 
